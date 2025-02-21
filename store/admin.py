a50496ea6cffcae34c9be5fca0d365c1899bbbf5
Mihail Sevostyanov <sevamihael141095@gmail.com>
2025-02-21 20:54:47 +0300

    Добавление моделей для store, дополнения

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Enterprise, Product
+
+
+@admin.register(Enterprise)
+class EnterpriseAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "level", "country", "city", "debt", "supplier")
+    list_filter = ("city", "country",)
+    actions = ('clear_debt',)
+
+    @admin.action(description="Очистить задолженность перед поставщиком")
+    def clear_debt(self, request, queryset):
+        queryset.update(debt=0.00)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "product_model", "release_date")
+    list_filter = ("supplier", "release_date")
+    search_fields = ("name", "supplier",)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import (CreateAPIView, RetrieveAPIView, ListAPIView, UpdateAPIView, DestroyAPIView)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from store.models import Enterprise, Product
+from store.permissions import IsActive
+from store.serializers import EnterpriseSerializer, ProductSerializer
+
+
+# Enterprises ##################################################################
+class EnterpriseCreateAPIView(CreateAPIView):
+    queryset = Enterprise.objects.all()
+    serializer_class = EnterpriseSerializer
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+class EnterpriseRetrieveAPIView(RetrieveAPIView):
+    queryset = Enterprise.objects.all()
+    serializer_class = EnterpriseSerializer
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+class EnterpriseListAPIView(ListAPIView):
+    queryset = Enterprise.objects.all()
+    serializer_class = EnterpriseSerializer
+    permission_classes = [IsAuthenticated, IsActive]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['country', 'city', 'street', 'house_number', 'level']
+
+
+class EnterpriseUpdateAPIView(UpdateAPIView):
+    queryset = Enterprise.objects.all()
+    serializer_class = EnterpriseSerializer
+    permission_classes = [IsAuthenticated, IsActive]
+
+    def perform_update(self, serializer):
+        if "debt" in serializer.validated_data:
+            serializer.validated_data.pop("debt")
+            raise Exception("Невозможно изменить поле задолженности")
+        super().perform_update(serializer)
+
+
+class EnterpriseDestroyAPIView(DestroyAPIView):
+    queryset = Enterprise.objects.all()
+    serializer_class = EnterpriseSerializer
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+# Products #####################################################################
+class ProductCreateAPIView(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+class ProductRetrieveAPIView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+class ProductListAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+class ProductUpdateAPIView(UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsActive]
+
+
+class ProductDestroyAPIView(DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsActive]
